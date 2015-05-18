07a221128211a9a0edea005d94ebc70cb89da57b
Cameron Strandberg <cameron.strandberg@gmail.com>
2015-05-18 12:48:19 -0700

    Added Product model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517230710) do
+ActiveRecord::Schema.define(version: 20150518192055) do
+
+  create_table "products", force: true do |t|
+    t.string   "title",      default: ""
+    t.decimal  "price",      default: 0.0
+    t.boolean  "published",  default: false
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
