d8700f5afdc62a1af9f67134c60382aeef1f5101
Cameron Strandberg <cameron.strandberg@gmail.com>
2015-05-17 18:22:52 -0700

    Add create session action

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  before_create :generate_authentication_token!
 
+  def generate_authentication_token!
+   begin
+     self.auth_token = Devise.friendly_token
+   end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ MarketPlaceApi::Application.routes.draw do
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api'}, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy]
+      resources :sessions, :only => [:create, :destroy]
     end
   end
 end
