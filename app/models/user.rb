f95488bbfaf678df0bfe4a2a5c02fd6ea0dd6972
Cameron Strandberg <cameron.strandberg@gmail.com>
2015-05-17 17:19:52 -0700

    Modified user model for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :auth_token, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,10 +6,12 @@ describe User do
   it {should respond_to(:email)}
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
+  it {should respond_to(:auth_token)}
 
   it { should be_valid }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+  it {should validate_uniqueness_of(:auth_token)}
 end
