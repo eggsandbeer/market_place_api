c1d9e13d719b0cd8edeb909d958a82dafc73faf8
Cameron Strandberg <cameron.strandberg@gmail.com>
2015-05-20 22:32:44 -0700

    Adds search scopes on the product model

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -8,8 +8,13 @@ describe Api::V1::ProductsController do
     end
 
     it "returns the information about a reporter on a hash" do
-      product_response = json_response
+      product_response = json_response[:product]
       expect(product_response[:title]).to eql @product.title
+    end
+
+    it "has the user as a embeded object" do
+      product_response = json_response[:product]
+      expect(product_response[:user][:email]).to eql @product.user.email
     end
 
     it {should respond_with 200}
@@ -20,10 +25,50 @@ describe Api::V1::ProductsController do
       4.times { FactoryGirl.create :product}
       get :index
     end
+    context "when is not receiving any product_ids parameter" do
+      before(:each) do
+        get :index
+      end
 
+      it "returns 4 records from the database" do
+        products_response = json_response
+        expect(products_response[:products]).to have(4).items
+      end
+
+      it "returns the user object into each product" do
+        products_response = json_response[:products]
+        products_response.each do |product_response|
+          expect(product_response[:user]).to be_present
+        end
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "when product_ids parameter is sent" do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        3.times { FactoryGirl.create :product, user: @user }
+        get :index, product_ids: @user.product_ids
+      end
+
+      it "returns just the products that belong to the user" do
+        products_response = json_response[:products]
+        products_response.each do |product_response|
+          expect(product_response[:user][:email]).to eql @user.email
+        end
+      end
+    end
     it "returns 4 records from the database" do
-      products_response = json_response
-      expect(products_response[:products]).to have(5).items
+      products_response = json_response[:products]
+      expect(products_response).to have(4).items
+    end
+
+    it "returns the user object into each product" do
+      products_response = json_response[:products]
+      products_response.each do |product_response|
+        expect(product_response[:user]).to be_present
+      end
     end
 
     it { should respond_with 200 }
@@ -39,7 +84,7 @@ describe Api::V1::ProductsController do
       end
 
       it "renders the json representation for the product record just created" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql @product_attributes[:title]
       end
 
@@ -81,7 +126,7 @@ describe Api::V1::ProductsController do
       end
 
       it "renders the json respresentatipon for the updated use" do
-        product_response = json_response
+        product_response = json_response[:product]
         expect(product_response[:title]).to eql "An expensive TV"
       end
 
@@ -115,6 +160,6 @@ describe Api::V1::ProductsController do
       delete :destroy, { user_id: @user.id, id: @product.id }
     end
 
-    it { should respond_with 204 }
+    it { should respond_with 204}
   end
 end
