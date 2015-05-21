fb09d9aa8f4ceb68081843147de8c4ba6df1f4b4
Cameron Strandberg <cameron.strandberg@gmail.com>
2015-05-20 20:49:15 -0700

    Products controller update actions

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -67,4 +67,43 @@ describe Api::V1::ProductsController do
       it { should respond_with 422 }
     end
   end
+
+  describe "PUT/PATCH #update" do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create :product, user: @user
+      api_authorization_header @user.auth_token
+    end
+
+    context "when is successfully updated" do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: {title: "An expensive TV"}}
+      end
+
+      it "renders the json respresentatipon for the updated use" do
+        product_response = json_response
+        expect(product_response[:title]).to eql "An expensive TV"
+      end
+
+      it {should respond_with 200}
+    end
+
+    context "when is not updated" do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: {price: "Two hungee dollar"}}
+      end
+
+      it "renders an errors json" do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user culd not b cre8d" do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include "is not a number"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
